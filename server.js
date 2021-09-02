ce3d24490db695fc22dcb975c37a61b13ba876aa
nickjang <nickjang234@gmail.com>
2021-09-02 10:36:07 +0700

    changed port

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-app.listen(8080, () => {
-	console.log(`Server started at ${8080}`.cyan);
+app.listen(8000, () => {
+	console.log(`Server started at ${8000}`.cyan);
 });
