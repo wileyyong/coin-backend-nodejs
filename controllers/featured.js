4183e46dad23ac114351ebd91661cfccfe971059
nickjang <nickjang234@gmail.com>
2021-10-08 23:00:47 -0600

    fixed

--- controllers/featured.js
+++ controllers/featured.js
@@ -1,4 +1,3 @@
-const Users = require("../models/users");
 const Featured = require("../models/featured");
 const helpers = require("../helpers");
 
@@ -11,8 +10,15 @@ const Controller = {
 
 			if (featured_name !== undefined) set.featured_name = featured_name;
 			if (featured_price !== undefined) set.featured_price = featured_price;
-
-			var update = await Featured.updateOne({created_by: 'admin'}, set);
+      var exit = await Featured.findOne({created_by: 'admin'});
+      if (exit) {
+        var update = await Featured.updateOne({created_by: 'admin'}, set);
+        res.send({message: "Featured updated"});
+      } else {
+        await Featured.create({ created_by: 'admin', featured: '', featured_name: set.featured_name, featured_price: set.featured_price });
+        res.send({message: "Featured added"});
+      }
+			
 			if (!update || !update.nModified)
 				return res.status(422).send({error: "Nothing was updated"});
 			
@@ -28,11 +34,15 @@ const Controller = {
 		try {
 			if (req.files && req.files.featuredImage) {
 				var featured = await helpers.uploadFile(req.files.featuredImage, req.user.id, "content/cover");
-				var update = await Featured.updateOne({created_by: 'admin'}, { $set: {featured} }).exec();
-
-				res.send({message: "Featured Image updated"});
-			}
-			else {
+        var exit = await Featured.findOne({created_by: 'admin'});
+        if (exit) {
+          var update = await Featured.updateOne({created_by: 'admin'}, { $set: {featured} }).exec();
+          res.send({message: "Featured Image updated"});
+        } else {
+          await Featured.create({ created_by: 'admin', featured: featured, featured_name: '', featured_price: 0 });
+          res.send({message: "Featured Image added"});
+        }
+			} else {
 				var update = await Featured.updateOne({created_by: 'admin'}, {$unset: {featured: true}}).exec();
 
 				if (!update || !update.nModified) {
@@ -50,10 +60,9 @@ const Controller = {
 
   async getFeatured(req, res) {
     try {
-      var featuredNFT = await Featured.find({});
-      console.log(featuredNFT);
+      var featuredNFT = await Featured.findOne({created_by: 'admin'});
       if (!featuredNFT) 
-				return res.status(404).send({error: "Token not found"});
+				return res.status(404).send({error: "Featured not found"});
       res.send(featuredNFT);
     } catch(error) {
       res.status(500).send({error: "Server error"});
