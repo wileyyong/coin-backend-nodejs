322450816545b540601c4e6f1cbbf3f07d01e2f5
nickjjang <nickjang234@gmail.com>
2022-02-28 22:41:13 +0300

    controller mysql token restored

--- controllers/mysql/tokens.js
+++ controllers/mysql/tokens.js
@@ -265,33 +265,12 @@ const Controller = {
 
 			if (!token) 
 				return res.status(404).send({error: "Token not found"});
-
-			var collection = await Collections.findOne({
-				where: {_id: token.collectionsId}
+			res.send({
+				name: token.name,
+				description: token.description,
+				attributes: JSON.parse(token.attributes),
+				image: token.thumbnail || token.media,
 			});
-
-			if (!collection) {
-				res.send({
-					name: token.name,
-					description: token.description,
-					attributes: JSON.parse(token.attributes),
-					image: token.thumbnail || token.media,
-				});
-			} else {
-				res.send({
-					name: token.name,
-					description: token.description,
-					attributes: JSON.parse(token.attributes),
-					image: token.thumbnail || token.media,
-					collection: collection
-				});
-			}
-			// res.send({
-			// 	name: token.name,
-			// 	description: token.description,
-			// 	attributes: JSON.parse(token.attributes),
-			// 	image: token.thumbnail || token.media,
-			// });
 		}
 		catch(error) {
 			console.log(error);
